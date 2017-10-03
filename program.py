d31fc6404609c74f6a5082ab5eef67fb8a149bd2
casseds2 <casseds95@gmail.com>
2017-10-03 16:55:53 +0100

    Hex and TIme

--- program.py
+++ program.py
@@ -9,9 +9,10 @@ def change(params):
 	x = []
 	for w in xrange(0, len(my_list), 2):
 		x.append(my_list[w])
+		#my_list.remove(my_list[w])
+		#print(my_list)
 	return x
 
 if __name__ == "__main__":
 	words = "This mostly is some text which I would want now to quickly simplify"
-	new_list = change(words)
-	print((' '.join(new_list)).upper())
+	print((' '.join(change(words))).upper())
