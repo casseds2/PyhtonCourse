2c7fad265c9407c7df769aab29fcb7c4ef58f6f8
casseds2 <casseds95@gmail.com>
2017-10-03 17:09:05 +0100

    Finsihed for Day

--- hexAndTime.py
+++ hexAndTime.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 import random
-import time
+import time		
 
 def myid():
-	i = hex(random.randint(0, 1000000))
+	i = hex(random.randint(0, 0xFFFFFFFF))
 	#print(str(i))
 	#print(len(str(time.time())))	
-	return "{:0>8}".format(i) + "-" + str(time.time())[:-5]
+	#return "{:0>8}".format(i) + "-" + str(time.time())[:-5]
+	#return "{0:08X}-{1:08X}".format(str(i), int(time.time()))
+	return str(i) + "-" + str(int(time.time()))
 
 if __name__ == "__main__":
 	print(myid())
