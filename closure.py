3871295acd264fd7f40ee7130df513a1c09ba663
casseds2 <casseds95@gmail.com>
2017-10-04 11:44:09 +0100

    Update

--- closure.py
+++ closure.py
@@ -1,10 +1,43 @@
 from __future__ import print_function
 
-def closureFunc():
+def closureFuncOne():
 	def print_string():
 		print(string)
-	string = "Hello World"
+	string = "Hello World One"
 	return print_string
 
+def closureFuncTwo():
+	def print_string_one():
+		def print_string_two():
+			print(stringTwo)
+		stringTwo = "World Two"
+		print(stringOne, end=" ")
+		return print_string_two
+	stringOne = "Hello"
+	return print_string_one
+
+def closureFuncThree():
+	def print_string_one():
+		def print_string_two():
+			def print_string_three():
+				print(stringThree)
+			stringThree = "Three"
+			print(stringTwo, end = " ")
+			return print_string_three
+		stringTwo = "World"
+		print(stringOne, end=" ")
+		return print_string_two
+	stringOne = "Hello"
+	return print_string_one
+
 if __name__ == "__main__":
-	closureFunc()()
+	closureFuncOne()()
+	closureFuncTwo()()()
+	closureFuncThree()()()()
+
+'''
+
+NOTE HOW STRING IS CARRIED WITH THE print_string() FUNCTION.
+THIS IS INBUILT FUNCTIONALITY WITH PYTHON WHERE IT WILL TAKE THE STRING
+
+'''
